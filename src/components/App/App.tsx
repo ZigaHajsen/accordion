e1d5ac7b6919620cf45d8db72219e7ada0ffba08
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-11-18 18:09:30 +0100

    Initial setup

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Question } from '../';
+
+const App = () => {
+  return (
+    <div>
+      App
+      <Question />
+    </div>
+  );
+};
+
+export default App;
